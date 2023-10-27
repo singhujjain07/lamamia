e6937d5cb7483136442f9f9ebbc6eaff2bc71a07
Harsh Singh <harshsinghujjain07@gmail.com>
2023-10-27 14:14:51 +0530

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+import Navbar from '@/components/header/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Footer from '@/components/footer/Footer'
+import { ThemeProvider } from '@/context/ThemeContext'
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +14,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider>
+          <AuthProvider>
+            <div className='container'>
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+            </AuthProvider>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>©2023 Lamamia, All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png"  height={15} width={15} className={styles.icon} alt="Lama Dev fb" />
+        <Image src="/2.png"  height={15} width={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/3.png"  height={15} width={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/4.png"  height={15} width={15} className={styles.icon} alt="Lama Dev" />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
